6d1a350a5ae120607ef76e54cb61186821e61924
youngje_woo <youngje_woo@tmax.co.kr>
2021-10-18 03:27:25 +0900

    [feat] 서버 개발 환경 구축
    - Node, TS, nodemon, pg 등 패키지 설치
    - 테스트용 웹 서버 구축

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// Server.ts
+import express from 'express'
+import { Client } from 'pg';
+
+const roo = new Client({
+    user: 'roo', // User 이름
+    database: 'blog', // DB 이름
+    password: '1', // 임시 비밀번호
+});
+
+roo.connect();
+
+roo.query('SELECT * FROM pg_catalog.pg_tables;', (err, res) => {
+    console.log(res);
+    roo.end();
+});
+
+
+class Server {
+    // app 타입 지정
+    public app: express.Application;
+
+    // 생성자
+    constructor() {
+        this.app = express();
+
+        this.app.get('/', function (req, res) {
+            res.send('Hello 스터D');
+        });
+    }
+}
+
+const server = new Server().app;
+
+server.set('port', 3000) // 포트지정 바로 listen으로 지정해도 상관없음
+
+server.listen(server.get('port'), () => {
+    console.log(`${server.get('port')} server is Running`)
+}).on('error', err => {
+    console.log(`Error message ${err}`);
+});
+
