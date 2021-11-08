e93cbc61799b63a36fc98fe70c0c235851baa472
youngje_woo <youngje_woo@tmax.co.kr>
2021-11-09 07:22:56 +0900

    [feat] USER 관련 API 구현
    - 유저 관련 API의 구현부를 DB를 활용할 수 있도록 변경
    - 기존 서버의 객체 활용하던 코드 제거
    - post용 API 파일 추가

--- src/router/userApi.ts
+++ src/router/userApi.ts
@@ -1,42 +1,7 @@
 import express from 'express';
 import { Client } from 'pg';
 
-interface UserInfoDTO {
-  user_id: number;
-  user_name: string;
-  velog_name: string;
-  social_info?: string;
-  email_addr?: string;
-  email_info?: string;
-}
-
 const router = express.Router();
-const temp: UserInfoDTO[] = [
-  {
-    user_id: 1,
-    user_name: 'youngje',
-    velog_name: 'youngje_woo',
-    social_info: '',
-    email_addr: '',
-    email_info: '',
-  },
-  {
-    user_id: 2,
-    user_name: 'chaemin',
-    velog_name: 'coolchaem',
-    social_info: '',
-    email_addr: '',
-    email_info: '',
-  },
-  {
-    user_id: 3,
-    user_name: 'yeojin',
-    velog_name: 'yeogenius',
-    social_info: '',
-    email_addr: '',
-    email_info: '',
-  },
-];
 const roo = new Client({
   user: 'roo', // User 이름
   host: '192.168.11.60',
@@ -48,106 +13,117 @@ const roo = new Client({
 roo.connect();
 
 // 사용자 목록
-router.get('/users', (req, res) => {
-  // 이 쿼리가 왜 안 되지..?
-  // DB와 gui 툴 간의 싱크가 맞지 않는 거 같다...ㅠ
-  // roo.query('SELECT * FROM pg_catalog.pg_tables', (err, res) => {
-  //   console.log(res);
-  //   roo.end();
-  // });
-  return res.json(temp);
+router.get('/users', (req, response) => {
+  // 테이블 이름에 "" 반드시 붙여줘야함..
+  roo.query('SELECT * FROM public."BLOG_USERS"', (err, result) => {
+    return response.json(result.rows);
+  });
 });
 
 // id에 해당하는 사용자 리턴
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', (req, response) => {
   // id가 Nullish 값인 경우
   const id = parseInt(req.params.id, 10);
   if (!id) {
-    return res.status(400).json({ err: 'Incorrect id!' });
+    return response.status(400).json({ err: 'Incorrect id!' });
   }
-  // id에 해당하는 사용자가 없는 경우
-  const user = temp.filter(user => user.user_id === id);
-  if (user.length === 0) {
-    return res.status(404).json({ err: 'Unknown user' });
-  }
-
-  return res.json(user[0]);
+  // id가 유효한 경우
+  roo.query('SELECT * FROM public."BLOG_USERS"', (err, result) => {
+    const users = result.rows;
+    const user = users.filter(user => user.user_id === id);
+    // id에 해당하는 사용자가 없는 경우
+    if (user.length === 0) {
+      return response.status(404).json({ err: 'Unknown user' });
+    }
+    return response.json(user[0]);
+  });
 });
 
 // id에 해당하는 사용자 삭제
-router.delete('/users/:id', (req, res) => {
+router.delete('/users/:id', (req, response) => {
   // id가 Nullish 값인 경우
   const id = parseInt(req.params.id, 10);
   if (!id) {
-    return res.status(400).json({ err: 'Incorrect id!' });
+    return response.status(400).json({ err: 'Incorrect id!' });
   }
-  // id에 해당하는 사용자가 없는 경우
-  const idx = temp.findIndex(user => user.user_id === id);
-  if (idx === -1) {
-    return res.status(404).json({ err: 'Unknown user' });
-  }
+  // id가 유효한 경우
+  roo.query('SELECT * FROM public."BLOG_USERS"', (err, result) => {
+    const users = result.rows;
 
-  temp.splice(idx, 1);
-  res.status(204).send();
+    // id에 해당하는 사용자가 없는 경우
+    const idx = users.findIndex(user => user.user_id === id);
+    if (idx === -1) {
+      return response.status(404).json({ err: `Unknown user ${id}`, log: users });
+    }
+
+    const deleteQuery = `DELETE FROM public."BLOG_USERS"
+    WHERE user_id IN
+    (${id});`;
+
+    // Delete 쿼리는 응답이 없나..?
+    roo.query(deleteQuery, (err, result) => {
+      response.status(204).send(`User ${id} DELETED`);
+    });
+  });
 });
 
 // 사용자 추가
-router.post('/users', (req, res) => {
-  // validation 필요할 수도
-  const userName = req.body.user_name || '';
-  const velogName = req.body.velog_name || '';
-  const socialInfo = req.body.social_info || '';
-  const emailAddr = req.body.email_addr || '';
-  const emailInfo = req.body.email_info || '';
+router.post('/users', (req, response) => {
+  roo.query('SELECT * FROM public."BLOG_USERS"', (err, result) => {
+    const users = result.rows;
 
-  const newId =
-    temp.reduce((maxId, user) => {
-      return user.user_id > maxId ? user.user_id : maxId;
-    }, 0) + 1;
+    // 새로운 user id 발급
+    const newId =
+      users.reduce((maxId, user) => {
+        return user.user_id > maxId ? user.user_id : maxId;
+      }, 1) + 1;
 
-  // 새로운 유저 정의
-  const newUser = {
-    user_id: newId,
-    user_name: userName,
-    velog_name: velogName,
-    social_info: socialInfo,
-    email_addr: emailAddr,
-    email_info: emailInfo,
-  };
-  temp.push(newUser);
+    // column name 스트링 생성 (req.body에 대한 validation 필요할 수도)
+    const targetColStr = ['user_id', ...Object.getOwnPropertyNames(req.body)]
+      .filter(prop => prop !== '')
+      .join(',');
+    // column value 스트링 생성
+    const newUserInfoStr = [newId, ...Object.values(req.body).map(val => `'${val}'`)]
+      .filter(prop => prop !== '')
+      .join(',');
+    // 새로운 유저 생성 쿼리
+    const insertQuery = `INSERT INTO public."BLOG_USERS" (${targetColStr}) 
+      VALUES (${newUserInfoStr}) returning user_id;`;
 
-  return res.status(201).json(newUser);
+    roo.query(insertQuery, (err, result) => {
+      console.log(err);
+      return response.status(201).json(`User ${newId} created`);
+    });
+  });
 });
 
 // 사용자 정보 업데이트
-router.put('/users/:id', (req, res) => {
+router.put('/users/:id', (req, response) => {
   // id가 Nullish 값인 경우
   const id = parseInt(req.params.id, 10);
   if (!id) {
-    return res.status(400).json({ err: 'Incorrect id!' });
+    return response.status(400).json({ err: 'Incorrect id!' });
   }
 
-  // id에 해당하는 사용자가 없는 경우
-  const idx = temp.findIndex(user => user.user_id === id);
-  if (idx === -1) {
-    return res.status(404).json({ err: 'Unknown user' });
-  }
-  const target = temp[idx];
+  roo.query('SELECT * FROM public."BLOG_USERS"', (err, result) => {
+    const users = result.rows;
+    // id에 해당하는 사용자가 없는 경우
+    const idx = users.findIndex(user => user.user_id === id);
+    if (idx === -1) {
+      return response.status(404).json({ err: 'Unknown user' });
+    }
 
-  const userName = req.body.user_name || '';
-  const velogName = req.body.velog_name || '';
-  const socialInfo = req.body.social_info || '';
-  const emailAddr = req.body.email_addr || '';
-  const emailInfo = req.body.email_info || '';
+    // set 스트링 생성
+    const setStr = Object.entries(req.body).map(e => `${e[0]} = '${e[1]}'`);
 
-  // 유저 정보 변경
-  target.user_name = userName;
-  target.velog_name = velogName;
-  target.social_info = socialInfo;
-  target.email_addr = emailAddr;
-  target.email_info = emailInfo;
+    // 새로운 유저 생성 쿼리
+    const updateQuery = `UPDATE public."BLOG_USERS" SET ${setStr} WHERE user_id = ${id};`;
+    console.log(updateQuery);
 
-  return res.status(201).json(target);
+    roo.query(updateQuery, (err, result) => {
+      return response.status(201).json(`User ${id} updated`);
+    });
+  });
 });
 
 export default router;
